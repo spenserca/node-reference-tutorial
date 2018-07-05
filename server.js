64f9b883acf35cb2150971520c61e32240b74f4e
Spenser Andreassen <spenserca@gmail.com>
2018-07-05 13:55:04 -0500

    Authentication

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const Router = require('koa-router');
+const jwt = require('koa-jwt');
+const jwksRsa = require('jwks-rsa');
 
 const app = new Koa();
 
+app.use(createAuthMiddleware().unless({path: '/hello'}));
 app.use(bodyParser());
 app.use(buildRouter().routes());
 app.listen(3000);
@@ -12,4 +15,14 @@ function buildRouter() {
     const router = new Router();
     router.get('/hello', require('./hello'));
     return router;
+}
+
+function createAuthMiddleware() {
+    return jwt({
+        secret: jwksRsa.koaJwtSecret({
+            cache: true,
+            jwksUri: `https://cognito-idp.us-east-2.amazonaws.com/${process.env.USER_POOL_ID}/.well-known/jwks.json`
+        }),
+        algorithms: ['RS256']
+    });
 }
